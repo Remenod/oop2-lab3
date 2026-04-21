c18cecaf4bbb002cf7e95e66a79b4ff69074d2ec
Remenod <remenod@gmail.com>
2026-04-21 23:25:28 +0300

    fix bugs

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 struct Button
 {
     bool already_pressed;
-    size_t value;
+    int value;
 };
 
 std::vector<Button> buttons;
 
-int create_buttons(size_t val_from, size_t val_to, size_t step)
+int create_buttons(int val_from, int val_to, int step)
 {
     static const int max_buttons = 1000;
     if (val_from > val_to)
@@ -19,7 +19,7 @@ int create_buttons(size_t val_from, size_t val_to, size_t step)
     if ((val_to - val_from) / step > max_buttons)
         return 2;
 
-    for (size_t i = val_from; i < val_to; i += step)
+    for (int i = val_from; i < val_to; i += step)
     {
         buttons.emplace_back(new Button{
             already_pressed : false,
@@ -31,12 +31,12 @@ int create_buttons(size_t val_from, size_t val_to, size_t step)
 
 int remove_buttons(int n)
 {
-    std::erase_if(buttons, [n](int x)
-                  { return x % n == 0; });
+    std::erase_if(buttons, [n](const Button &b)
+                  { return b.value % n == 0; });
     return 0;
 }
 
-size_t check_is_prime(size_t number)
+int check_is_prime(int number)
 {
     if (number < 2)
         return number;
@@ -44,7 +44,7 @@ size_t check_is_prime(size_t number)
     if (number % 2 == 0)
         return (number == 2 ? number : 2);
 
-    for (size_t i = 3; i * i <= number; i += 2)
+    for (int i = 3; i * i <= number; i += 2)
     {
         if (number % i == 0)
             return i;
